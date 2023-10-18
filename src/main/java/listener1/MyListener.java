487433bd96ffe845619867cea1b3c042292341aa
QA-Dmytro <Vasilina.dmitrii@gmail.com>
2023-10-18 16:32:35 +0300

    Added listenre

--- src/main/java/listener1/MyListener.java
+++ src/main/java/listener1/MyListener.java
@@ -5,7 +5,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-public class Listener implements ITestListener {
+public class MyListener implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult result) {
